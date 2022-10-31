acf71077e4c52ab85d0b999d117c1f71e3584c03
griffinarnone <griffinarnone@192.168.0.47>
2022-10-31 13:27:35 -0400

    Continue to fix Projectile, Ball, Beam

--- src/things/Beam.java
+++ src/things/Beam.java
@@ -6,20 +6,15 @@ public class Beam extends Projectile {
 
 	@Override
 	public Rectangle getHitBox() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void handleCollision(Collidable collidable) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void move() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	
--- src/things/Projectile.java
+++ src/things/Projectile.java
@@ -4,11 +4,19 @@ import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import ui.GameView;
 
 public abstract class Projectile implements Collidable, Movable  {
 
+	public abstract void move();
 	
+	public abstract Rectangle getHitBox();
 	
+	public abstract double getXCoordinate();
+	
+	public abstract double getYCoordinate();
+	
+	public abstract double getSelf();
 	
 }
